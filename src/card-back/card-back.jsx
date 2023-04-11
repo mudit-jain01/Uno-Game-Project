c18bd065ebcc4c10fdef7bd3622e2783974bd655
aditirai2306 <71453235+aditirai2306@users.noreply.github.com>
2023-04-12 00:58:37 +0530

    Update card-back.jsx

--- src/card-back/card-back.jsx
+++ src/card-back/card-back.jsx
@@ -8,7 +8,7 @@ export default function cardback() {
         <React.Fragment>
             <div className="card">
                 <span className="inner">
-                    <img className="unologo" src="https://i.pinimg.com/736x/94/9d/84/949d84f3b506f3e9aaf83321551864d2.jpg">
+                    <img className="unologo" src="https://i.pinimg.com/736x/94/9d/84/949d84f3b506f3e9aaf83321551864d2.jpg" />
                 </span>
             </div>
         </React.Fragment>
