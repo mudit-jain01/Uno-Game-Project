4d302b0953d540a36aa3134c4987a008a005f514
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2023-04-12 13:17:28 +0530

    File structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState, useRef, useLayoutEffect, useEffect } from "react";
 import PlayScreen from "./screens/playground/PlayScreen";
 import ErrorChecks from "./components/ErrorDiv/CardError";
 import ChooseColor from "./components/CardColorChoice/CardColorChoice";
+import Roomselect from "./screens/room-select/roomselect";
+import Lobby from "./screens/lobby/lobby";
 import anime from "animejs";
 function App() {
   // function animate1(event) {
---
+++ src/screens/lobby/lobby.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./lobby.css";
+
+export default function Lobby(props) {
+  return (
+    <React.Fragment>
+      <div className="lobby-screen" id="244:847">
+        <div className="rectangle-27" />
+        <img className="rectangle-28" src="" alt="img" />
+        <img className="rectangle-30" src="" />
+        <img className="rectangle-31" src="" />
+        <img className="rectangle-32" src="" />
+        <img className="rectangle-33" src="" />
+        <div className="player-1-1">Player 1</div>
+        <div className="player-1-2">Player 1</div>
+        <div className="player-1-3">Player 1</div>
+        <div className="player-1-4">Player 1</div>
+        <div className="player-1-5">Player 1</div>
+        <div className="lobby">Lobby</div>
+        <div className="rectangle-25" />
+        <div className=" public">Public</div>
+      </div>
+    </React.Fragment>
+  );
+}
